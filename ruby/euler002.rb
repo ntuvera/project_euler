659dd4889c13f52d3d414bd622417551bf76c2b9
ntuvera <ntuvera@gmail.com>
2014-07-29 22:36:21 -0400

    ruby euler002

---
+++ ruby/euler002.rb
@@ -0,0 +1,16 @@
+def fibonacci_even
+  array =[0,1]
+  sum = 0
+  max = 4_000_000
+  while array[-1] < max
+    next_num = array[-1] + array[-2]
+    array.push(next_num)
+  end
+  array.each do |x|
+    if x % 2 == 0
+      sum += x
+    end
+  end
+  puts sum
+end
+
